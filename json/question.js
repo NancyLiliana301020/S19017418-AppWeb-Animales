7a8f8c6a7b65416222d457beb758285efd963bce
NancyLiliana301020 <nancylili301020@gmail.com>
2022-11-30 09:16:16 -0600

    Primer commit

---
+++ json/question.js
@@ -0,0 +1,105 @@
+data = [
+    {
+        question: "¿Que animal tiene 47 dientes?", 
+        answers : {
+        a:"Tiburon", 
+            b :"Mosquito", 
+            c: "Nutria", 
+            d: "Cotorro" },
+            correctAnswer: "b"
+        },
+    
+        {
+            question: "¿ Pueden usar su cola para comunicarse entre ellos", 
+            answers : 
+            {a:"Elefantes",
+             b: "Tiburon", 
+             c: "Zorro", 
+             d: "Lémures" },
+             correctAnswer:"c"
+        },
+        {
+            question: "¿Su corazón pesa más de 11 kg", 
+            answers : 
+            {a:"Elefantes",
+             b: "Jirafa", 
+             c: "Jaguar", 
+             d: "Jabali" },
+             correctAnswer:"b"
+        },
+        {
+            question: "¿Cuál de los siguientes animales no es un marsupial?", 
+            answers : 
+            {a:"Koala",
+             b: "Zarigueya", 
+             c: "Jaguar", 
+             d: "Perezozo" },
+             correctAnswer:"d"
+        },
+        {
+            question: "¿Cuál es el animal más venenoso del mundo?", 
+            answers: 
+            {a:"Medusa",
+             b: "Alacran", 
+             c: "Serpiente de cascbel", 
+             d: "Tiburon" },
+             correctAnswer:"a"
+        },
+        {
+            question: "¿El estómago de cuál de estos animales está dividido en cuatro compartimentos?", 
+            answers : 
+            {a:"Zorro",
+             b: "Cabra", 
+             c: "Leopardo", 
+             d: "Puma" },
+             correctAnswer:"b"
+        },
+        {
+            question: "¿De qué color es la piel de un oso polar?", 
+            answers : 
+            {a:"Blanca",
+             b: "Rosa", 
+             c: "Negra", 
+             d: "Azul" },
+             correctAnswer:"b"
+        },
+        {
+            question: "¿Cuál de estas especies no puede caminar?", 
+            answers : 
+            {a:"Mosca",
+             b: "Libelula", 
+             c: "Abeja", 
+             d: "Abeja" },
+             correctAnswer:"b"
+        },
+    
+         {
+            question: "¿Cuál de estos animales puede dormir hasta 22 horas por día?", 
+            answers: 
+            {a:"Koala",
+             b: "Lechuza", 
+             c: "Abeja", 
+             d: "Murcielago" },
+             correctAnswer:"a"
+        },
+        
+        {
+            question: "¿Cuál de estas aves vive más tiempo?", 
+            answers: 
+            {a:"Canario",
+             b: "Guacamayo", 
+             c: "Loro gris", 
+             d: "Aguila" },
+             correctAnswer:"c"
+        },
+    
+        {
+            question: "¿Qué animales les ponen nombres a sus crías?", 
+            answers: 
+            {a:"Delfines",
+             b: "Tiburones", 
+             c: "Osos", 
+             d: "Ballenas" },
+             correctAnswer:"a"
+        }
+];
